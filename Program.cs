0a6346ce10bacf330478d5621b28ace06fde44df
CommCode <dennis.hartstock@web.de>
2024-10-04 04:36:31 +0200

    add task solution

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string? input;
+int number;
+bool isValidNumber = false;
+
+do
+{
+    Console.WriteLine("Enter an integer value between 5 and 10:");
+
+    input = Console.ReadLine();
+    _ = int.TryParse(input, out number);
+
+    if (number >= 5 && number <= 10)
+    {
+        isValidNumber = true;
+        Console.WriteLine($"Your input value ({number}) has been accepted.");
+    } else if (number != 0)
+    {
+        Console.WriteLine($"You entered {number}. Please enter a number between 5 and 10");
+    } else
+    {
+        Console.WriteLine("Sorry, you entered an invalid number. Please try again");
+    }
+
+} while (!isValidNumber);
